0e627a18df52d3c15fd1a639dc3d8432eaffceb0
Joseph R. Lewis <30529+sanbeiji@users.noreply.github.com>
2026-06-06 21:36:28 -0700

    Fix Android TTS string template, add internet permission, update .gitignore and remove build artifacts

--- android/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_sanbeiji_shakespeareaninsults_InsultApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.sanbeiji.shakespeareaninsults.InsultApp"
-)
-public final class _com_sanbeiji_shakespeareaninsults_InsultApp {
-}
